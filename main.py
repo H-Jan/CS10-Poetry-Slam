606e9f2fd0586a97c05086347e331773fb73343a
Hani Jandali <hanijandali@hanis-mbp.lan>
2020-09-30 23:12:16 -0700

    Text Files/Challenge 1
    The .txt files through which we saved our poems can now be opened in the main.py file and be read.

--- main.py
+++ main.py
@@ -1,2 +1,50 @@
 
-# This is a new python file, testing 
+
+#Let us Begin! Project Requirements
+#CHALLENGE 1
+#You will store your poem in a .txt file. Feel free to use any poem of your choice!
+
+#CHALLENGE 2
+#Create a function called get_file_lines():
+
+#It should have 1 parameter called filename, which is a string of the filename.
+#It should return a list of strings containing the lines of the file.
+#Create a function called lines_printed_backwards():
+
+#CHALLENGE 3
+#It should have 1 parameter called lines_list, which is a list of strings containing lines of your poem.
+
+#It should print the lines of the poem in reverse. Include the original line number at the beginning of each line.
+
+#CHALLENGE 4
+#Create a function called lines_printed_random():
+
+#It should have 1 parameter called lines_list, which is a list of strings containing lines of your poem.
+#It should print the lines of your poem in randomly order. Repeats are ok, but make sure the number of lines 
+#printed is equal to the original lines in the poem (Line numbers do not need to be printed.) Hint: try using a loop 
+# and randint() from the random module.
+#Create a function called lines_printed_custom():
+
+#It should minimally have 1 parameter called lines_list, which is a list of strings containing lines of your poem.
+#It should print the poem in a unique way, be creative!
+#Make sure that you carefully comment your custom function so it’s clear what it does.
+
+#CHALLENGE 5
+#Your final submitted code should use the four functions (get_file_lines(), lines_printed_backwards(), 
+# lines_printed_random(), and lines_printed_custom()) you wrote. It should print out the poem contained in your text file backwards, 
+# random, and custom to the terminal.
+
+# STRETCH
+#Modify your program to write your poems to a file.
+#Modify your program to have a menu that the user can select from rather than printing all the options at once
+#Modify your program to read the poem from user input
+#Modify your program to randomly rearrange the words on each line
+
+The_file = open("PNPoem.txt")
+
+get_file_lines = The_file.readlines()
+for line in get_file_lines:
+    print(line)
+
+The_file.close()
+
